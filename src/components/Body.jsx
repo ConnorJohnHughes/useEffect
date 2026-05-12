0bdae526ab7400e7dfb7efe9cc4ad0e4122fea59
Connor <connorjohnhughes@gmail.com>
2026-05-12 10:58:04 -0700

    Added Body.jsx

---
+++ src/components/Body.jsx
@@ -0,0 +1,13 @@
+import './Body.css'
+
+export default function Body(){
+    return(
+    <>
+        <h1 className='body-h1'>This is the body!</h1>
+        <div className="body-container">
+
+        </div>
+
+    </>  
+    )
+}
