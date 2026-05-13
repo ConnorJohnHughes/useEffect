6aa9208824a1527caac28385dfe492ec12b36107
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-05-12 21:24:45 -0700

    fetched Pokemon data using useEffect and PokeAPI

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,6 +1,19 @@
+import { useState, useEffect } from 'react'
 import '../components/Body.css'
 
 export default function Body(){
+
+    const [allPokemon, setAllPokemon] = useState([])
+
+    useEffect(() => {
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.results)
+            setAllPokemon(data.results)
+        })
+    }, [])
+
     return(
     <div className="body-container">
         <h1 className='body-h1'>This is the body!</h1>
